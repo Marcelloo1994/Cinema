14781dd29afe2ab8d3954d1ad746723492ae44cb
Marcelloo1994 <114481672+Marcelloo1994@users.noreply.github.com>
2022-10-20 10:23:53 +0200

    First commit

---
+++ src/test/java/esame/java/cinema/ApplicationTests.java
@@ -0,0 +1,13 @@
+package esame.java.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
